7cdbdd1b5da4504d02f9d5d29eed79291c31f109
Vishal Gulati <vishal.rvg@gmail.com>
2019-03-28 10:11:11 +0530

    WIP - search response in msg

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 const GOOGLE_MAP_API = 'https://maps.googleapis.com/maps/api/js?key=';
 
-const GOOGLE_API_KEY = 'AIzaSyD2D0r1DXCw-EMAB1xTlc4SFnsN5z7CGx4';
+const GOOGLE_API_KEY = '';
 
 export const GOOGLE_API_URL = GOOGLE_MAP_API + GOOGLE_API_KEY;
