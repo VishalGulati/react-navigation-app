e2d1c1c16e4ee35d19216742f60a49f4825c264f
Vishal Gulati <vishal.rvg@gmail.com>
2019-03-29 08:49:31 +0530

    Updates

---
+++ precache-manifest.83e44269fb2153204acc9cefc868853e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c1a200fe380fb55679",
+    "url": "/react-navigation-app/static/js/runtime~main.51e675eb.js"
+  },
+  {
+    "revision": "d7f1d7c63469a155e908",
+    "url": "/react-navigation-app/static/js/main.0fe29429.chunk.js"
+  },
+  {
+    "revision": "8b351e4719efba1e54a8",
+    "url": "/react-navigation-app/static/js/2.1a835ba9.chunk.js"
+  },
+  {
+    "revision": "d7f1d7c63469a155e908",
+    "url": "/react-navigation-app/static/css/main.44e6919f.chunk.css"
+  },
+  {
+    "revision": "8b351e4719efba1e54a8",
+    "url": "/react-navigation-app/static/css/2.0a9a8073.chunk.css"
+  },
+  {
+    "revision": "c028ae29f9bb782ef269b0aa57d4793d",
+    "url": "/react-navigation-app/index.html"
+  }
+];
