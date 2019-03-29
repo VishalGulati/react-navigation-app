b6080f3a8fa5c1524ea343e67c5bc8b31a920412
Vishal Gulati <vishal.rvg@gmail.com>
2019-03-29 13:23:00 +0530

    API key moved to env file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,4 @@
 const GOOGLE_MAP_API = 'https://maps.googleapis.com/maps/api/js?key=';
 
-const GOOGLE_API_KEY = 'AIzaSyD2D0r1DXCw-EMAB1xTlc4SFnsN5z7CGx4';
-
-export const GOOGLE_API_URL = GOOGLE_MAP_API + GOOGLE_API_KEY;
+export const GOOGLE_API_URL =
+  GOOGLE_MAP_API + process.env.REACT_APP_GOOGLE_API_KEY;
