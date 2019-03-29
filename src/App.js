ffa0795f314e232b0078ef72fe4936eec5053b7c
Vishal Gulati <vishal.rvg@gmail.com>
2019-03-29 12:15:29 +0530

    App converted to functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader/AppHeader';
 import AppBody from './containers/AppBody/AppBody';
 import AppFooter from './components/AppFooter/AppFooter';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppHeader />
-        <AppBody />
-        <AppFooter />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
+    </div>
+  );
 }
 
 export default App;
